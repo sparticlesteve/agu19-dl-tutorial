f94b1b254a31878f3645f5418e5c8f60c503ce05
Steve Farrell <sparticlesteve@gmail.com>
2019-12-04 21:50:23 -0800

    updated session config to compat.v1 module paths to suppress deprecation warnings

--- utils/device.py
+++ utils/device.py
@@ -7,18 +7,20 @@ import os
 
 # Externals
 import tensorflow as tf
+from tensorflow.compat.v1 import ConfigProto, Session, keras
 
 def configure_session(intra_threads=4, inter_threads=2,
                       blocktime=1, affinity='granularity=fine,compact,1,0',
                       gpu=None):
     """Sets the thread knobs in the TF backend"""
+
     os.environ['KMP_BLOCKTIME'] = str(blocktime)
     os.environ['KMP_AFFINITY'] = affinity
     os.environ['OMP_NUM_THREADS'] = str(intra_threads)
-    config = tf.ConfigProto(
+    config = ConfigProto(
         inter_op_parallelism_threads=inter_threads,
         intra_op_parallelism_threads=intra_threads
     )
     if gpu is not None:
         config.gpu_options.visible_device_list = str(gpu)
-    tf.keras.backend.set_session(tf.Session(config=config))
+    keras.backend.set_session(Session(config=config))
